4fa2cfeb10c8c2a498f265c9f83f72d029daff55
Nam Pham <nampnq@gmail.com>
2016-01-28 13:53:54 +0700

    Add multiple-cursors

--- init.el
+++ init.el
@@ -377,6 +377,15 @@
 (use-package php-mode
   :ensure t)
 
+;; multiple-cursors
+(use-package multiple-cursors
+  :ensure t
+  :bind
+  ("C-S-c C-S-c" . mc/edit-lines)
+  ("C->" . mc/mark-next-like-this)
+  ("C-<" . mc/mark-previous-like-this)
+  ("C-c C-<" . mc/mark-all-like-this))
+
 ;;;;;;;;;;;;;;;;;;;;
 ;; Hook Functions ;;
 ;;;;;;;;;;;;;;;;;;;;
